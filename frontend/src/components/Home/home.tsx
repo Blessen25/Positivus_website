8ed6a48f0e5394e00e72a27820181b568b90b88e
Blessen25 <blessenvinoymathew25@gmail.com>
2026-07-11 13:18:32 +0300

    First Section Desktop version finished

--- frontend/src/components/Home/home.tsx
+++ frontend/src/components/Home/home.tsx
@@ -93,13 +93,14 @@ const Home_firstSection:React.FC = () => {
 
                 <div className="home_first_Contentsec">
 
-                    <h1 className="h1" >Navigating the digital landscape for success</h1>
-                    <p className="text-md">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+                    <h1 className="navigatecontent" >Navigating the digital landscape for success</h1>
+                    <p className="text-bg">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
                     <Button_Comp_Clr text="Book a Consultation" className="bookaconsclass"/>
                 </div>
-                <div className="home_first_Imagesec">
+                <div className="home_first_Imagesec">   
 
-                </div>
+                    <img src="../../src/assets/images/home_one_sec.png" alt="home_firstsection" className="home_firstsection_img" loading="lazy"/>
+                </div>  
             </div>
         </>
     )
